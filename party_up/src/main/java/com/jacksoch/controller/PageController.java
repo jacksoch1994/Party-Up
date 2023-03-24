c2fbce009f8b1121bf0e75119582a6b60ef7d1fb
Christopher Jackson <jacksoch1994@gmail.com>
2023-03-24 17:35:57 -0400

    Create game-postings.html and connected database resource to page.

--- party_up/src/main/java/com/jacksoch/controller/PageController.java
+++ party_up/src/main/java/com/jacksoch/controller/PageController.java
@@ -5,11 +5,13 @@ import com.jacksoch.dao.JoinRequestDao;
 import com.jacksoch.dao.MessageDao;
 import com.jacksoch.dao.UserDao;
 import com.jacksoch.model.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class PageController {
@@ -42,7 +44,24 @@ public class PageController {
 
     @PostMapping("/register")
     public String newUser(@ModelAttribute("user") User user) {
-        System.out.println(user);
+
+        if (userDao.getUserByEmail(user.getEmail()) != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This email is already in use.");
+        }
+
+        if (userDao.getUserByUsername(user.getUsername()) != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This username is already in use.");
+        }
+
+        userDao.createUser(user);
         return "index";
+    }
+
+    @GetMapping("/games")
+    public String games(Model model) {
+        //Adds all groups to the page as an 'attribute' to be accessed by Thymeleaf when rendering HTML
+        model.addAttribute("groups", groupDao.getAllGroups());
+
+        return "game-postings";
     }
 }
